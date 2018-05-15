1dbca9bbebb44dbc6fcf85c23b30c35764f797dc
piotermiarer <piotr@atopik.pl>
2018-05-15 21:53:28 +0200

    Initial commit

---
+++ wedding_guests/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse('result')
