b98e112df9c9424619c0151c342b54cd5526e926
piotermiarer <piotr@atopik.pl>
2018-05-16 21:15:33 +0200

    Add Guest model, do some cleaning

--- wedding_guests/models.py
+++ wedding_guests/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Guest(models.Model):
+    YES, NO, MAYBE = 'Yes', 'No', 'Maybe'
+    ATTENDING_STATUSES = (
+        (YES, 'Tak'),
+        (NO, 'Nie'),
+        (MAYBE, 'Nie określono')
+    )
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField('Imię', max_length=30)
+    surname = models.CharField('Nazwisko', max_length=50)
+    attending = models.CharField(
+        'Będę na weselu', max_length=5, default=MAYBE, choices=ATTENDING_STATUSES
+    )
+    wants_bus = models.BooleanField(
+        'Chcę jechać autobusem',
+        default=False,
+        help_text='''Zapewniamy dojazd spod kościoła na salę weselną zaraz po mszy
+        i z powrotem do Poznania na koniec wesela (koło 4 w nocy).'''
+    )
+    is_vegetarian = models.BooleanField(
+        'Preferuję dania wegetariańskie',
+        default=False,
+        help_text='Informacja ta pomoże nam przy wyborze menu'
+    )
+    gift = models.CharField('Prezent', blank=True, editable=False, max_length=50)
+    comments = models.TextField('Dodatkowy komentarz', blank=True, max_length=200)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.surname)
--- wedding_guests/views.py
+++ wedding_guests/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.views import View
-from django.http import HttpResponse
 
 
-class Home(View):
-    def get(self, request):
-        return render(request, 'home.html')
+class Home(TemplateView):
+    template_name = 'home.html'
