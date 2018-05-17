5d28e6cb30fde568d87a4eb445d42ff9bad38b40
piotermiarer <piotr@atopik.pl>
2018-05-17 10:22:15 +0200

    Add simple login

--- wedding_guests/urls.py
+++ wedding_guests/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
+    path('login', views.Login.as_view(), name='login'),
+    path('guest', views.Guest.as_view(), name='guest'),
     path('', views.Home.as_view(), name='home')
 ]
--- wedding_guests/views.py
+++ wedding_guests/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login
+
 from django.views import View
 
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+
+class Guest(TemplateView):
+    template_name = 'guest.html'
+
+
+class Login(View):
+
+    def get(self, request):
+        return redirect('home')
+
+    def post(self, request):
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('guest')
+        else:
+            return redirect('home')
