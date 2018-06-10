695967068ef81bf8969b1c9aabc2cea8bae2477b
piotermiarer <piotr@atopik.pl>
2018-06-10 12:12:55 +0200

    Add similar gifts

--- wedding_guests/admin.py
+++ wedding_guests/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Guest
 
 
-class GuestAdmin(admin.ModelAdmin):
-    exclude = ('gift',)
+#class GuestAdmin(admin.ModelAdmin):
+#    exclude = ('gift',)
+#
+#admin.site.register(Guest, GuestAdmin)
 
-admin.site.register(Guest, GuestAdmin)
+admin.site.register(Guest)
--- wedding_guests/models.py
+++ wedding_guests/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+import editdistance as ed
+
+STOP_WORDS = ['do', 'w', 'dla', 'na', 'i', 'o', 'we', 'u']
+SIMILARITY_THRESHOLD = 0.7
+
 
 class Guest(models.Model):
     YES, NO, MAYBE = 'Yes', 'No', 'Maybe'
@@ -31,7 +36,30 @@ class Guest(models.Model):
 
     @staticmethod
     def get_similar_gifts(gift):
-        return ', '.join([gift, 'czołg', 'odrzutowiec'])
+        gifts = Guest.objects.order_by('gift').values_list('gift', flat=True).distinct()
+        gift_scores = []
+        for old_gift in gifts:
+            gift_scores.append((old_gift, Guest.compare_gifts(old_gift, gift)))
+        print(gift_scores)
+        suggestions = map(
+            lambda x: x[0],
+            filter(lambda x: x[1] > SIMILARITY_THRESHOLD, gift_scores)
+        )
+        return ', '.join(suggestions)
+
+    @staticmethod
+    def compare_gifts(gift1, gift2):
+        gift1 = Guest.filter_stop_words(gift1)
+        gift2 = Guest.filter_stop_words(gift2)
+        scores = [
+            1 - ed.eval(w1, w2) / max(len(w1), len(w2)) for w2 in gift2 for w1 in gift1
+        ]
+        print(gift1, gift2, scores)
+        return max(scores)
+
+    @staticmethod
+    def filter_stop_words(text):
+        return list(filter(lambda w: w not in STOP_WORDS, text.split()))
 
     def __str__(self):
         return '{} {}'.format(self.name, self.surname)
