b6c256b3a8676493f2a05bcf6d7d800c1691f82d
piotermiarer <piotr@atopik.pl>
2018-06-13 09:40:54 +0200

    Add images to subpages and make it prettier

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -123,8 +123,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'images')
