364a3779fdab2e684a060855e021da6370c0c751
Piotr Miara <piotr@atopik.pl>
2018-12-03 20:33:28 +0100

    Add first translations

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,9 +43,24 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
+# Provide a lists of languages which your site supports.
+LANGUAGES = (
+    ('pl', _('Polish')),
+    ('en', _('English')),
+)
+
+# Set the default language for your site.
+LANGUAGE_CODE = 'pl'
+
+# Tell Django where the project's translation files should be.
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
