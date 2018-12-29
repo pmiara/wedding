e6e195e6dbf7f35baada05c8def8d3435079b637
Piotr Miara <piotr@atopik.pl>
2018-12-29 13:57:03 +0100

    Make settings modular. Split it to: base_settings, dev_settings and prod_settings

---
+++ ai_project/settings/__init__.py
@@ -0,0 +1,2 @@
+from .base_settings import *
+from .dev_settings import *
--- ai_project/settings/base_settings.py
+++ ai_project/settings/base_settings.py
@@ -18,15 +18,6 @@ from django.utils.translation import ugettext_lazy as _
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!9!iseevn5j6+!31=4=k!z!)zt77oewoy42+-s-7+1%enbi3lq'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -88,22 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/home/miara/wedding/ai_project/mysql.cnf',
-        },
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = []
 
