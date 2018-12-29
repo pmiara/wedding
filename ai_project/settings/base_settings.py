8cb5a230d43031edf76199345497cded52754b6e
Piotr Miara <piotr@atopik.pl>
2018-12-29 15:18:47 +0000

    Add '*' to ALLOWED_HOSTS

--- ai_project/settings/base_settings.py
+++ ai_project/settings/base_settings.py
@@ -17,7 +17,7 @@ from django.utils.translation import ugettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
