071af12b7303ce15938e2368a49c7bb203eb961e
Piotr Miara <piotr@atopik.pl>
2018-12-30 22:18:10 +0100

    Change location of stylesheet files in scss_watchdog

--- scss_watchdog.sh
+++ scss_watchdog.sh
@@ -1,1 +1,1 @@
-sass --watch wedding_guests/static/css/style.scss:wedding_guests/static/css/style.css --style compressed
+sass --watch wedding_guests/static/wedding_guests/css/style.scss:wedding_guests/static/wedding_guests/css/style.css --style compressed
