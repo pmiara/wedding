4ded8cc818e03435fd4ab61a7d358de0adff8c2f
kasztanka <m.lipowska@onet.pl>
2019-02-10 23:04:41 +0100

    Add more values and filters in guest list in admin

--- wedding_guests/admin.py
+++ wedding_guests/admin.py
@@ -4,7 +4,8 @@ from .models import Guest, Gift
 
 
 class GuestAdmin(admin.ModelAdmin):
-    pass #exclude = ('gift',)
+    list_display = ('__str__', 'attending', 'attending_afters', 'wants_bus', 'is_vegetarian', 'comments')
+    list_filter = ('attending', 'wants_bus', 'is_vegetarian', 'attending_afters', 'eligible_for_afters')
 
 
 admin.site.register(Guest, GuestAdmin)
